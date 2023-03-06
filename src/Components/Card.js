44059b8d634911dc3a6cea54e2002d6382ab2e0d
Mildred <mildredkoskei01@gmail.com>
2023-03-06 20:17:54 +0300

    :sparkles: new feature

---
+++ src/Components/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+const Card = (portfolio) =>{
+    return(
+        <>
+        <div className="container">
+            <div>
+                <h1>MILDRED Koskei</h1>
+
+            </div>
+        </div>
+        </>
+    )
+}
+export default Card;
