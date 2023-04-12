856515d74a3c6998f7f249554c273d6f5d94c315
Mildred <mildredkoskei01@gmail.com>
2023-04-12 15:23:28 +0300

    :construction: modified file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import './App.css';
-import './Components/Style.css';
-import React from 'react'
+import "./App.css";
+import "./Components/Style.css";
+import React from "react";
 import Navbar from "./Components/Navbar";
 import About from "./Components/About/About";
 import Home from "./Components/Home/Home";
 import Projects from "./Components/Projects/Projects";
 import Resume from "./Components/Resume/Resume";
 import Footer from "./Components/Footer";
-import { Route, Routes, BrowserRouter as Router } from "react-router-dom"
-
+import { Route, Routes, BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Navbar />
-      <Routes>
-      <Route path="/" element={<Home />} />
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
-      </Routes>
-   <Footer />
-    </div>
+        </Routes>
+        <Footer />
+      </div>
     </Router>
   );
 }
