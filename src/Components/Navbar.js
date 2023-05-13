97e0a2bbc908b615966826b66b4f9210aca8868e
Mildred <mildredkoskei01@gmail.com>
2023-05-13 13:44:58 +0300

    :construction: created navbar files

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,14 @@
 import React from "react";
 function Navbar(){
     return(
-        <div>Navbar</div>
+        <nav>
+            <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#resume">Resume</a></li>
+            </ul>
+        </nav>
     )
 }
 export default Navbar
